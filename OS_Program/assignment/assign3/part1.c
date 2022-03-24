ae63b84cbffdcc5c6a145e5c11d05c071fe66a52
UniverseJason <011203jasonlee@gmail.com>
2022-03-24 17:58:58 -0500

    Deleted

---
+++ OS_Program/assignment/assign3/part1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    if(argc < 3)
+    {
+        fprintf(stderr, "Usage: ./part1 infile outfile.\n");
+        return 1;
+    }
+}
