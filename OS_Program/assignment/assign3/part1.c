4b58be5f59da7b40e07f3c4fa7782c5c6dc2199d
UniverseJason <011203jasonlee@gmail.com>
2022-04-01 16:55:16 -0500

    updated

--- OS_Program/assignment/assign3/part1.c
+++ OS_Program/assignment/assign3/part1.c
@@ -3,19 +3,19 @@
 
 int main(int argc, char* argv[])
 {
-    // if(argc < 3)
-    // {
-    //     fprintf(stderr, "Usage: ./part1 infile outfile.\n");
-    //     return 1;
-    // }
+    if(argc < 3)
+    {
+        fprintf(stderr, "Usage: ./part1 infile outfile.\n");
+        return 1;
+    }
 
     int i=0;
 
     unsigned long LA, PA;
-    unsigned int PT[32]={2,4,1,7,3,5,6};
-    
+
     // set other PT list index  to 0
-    for(i=7; i<=32; i++)
+    unsigned int PT[32]={2,4,1,7,3,5,6};
+    for(i=7; i<32; i++)
     {
         PT[i]=0;
     }
@@ -24,19 +24,30 @@ int main(int argc, char* argv[])
     unsigned int pnum, fnum, dnum;
 
     // open the file 
-    FILE *inFile = fopen("part1testsequence","rb");
+    FILE *inFile = fopen(argv[1],"rb");
     if(inFile == NULL)
     {
         fprintf(stderr, "ERROR: can not read the file\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    // read file 
-    while( fread(&LA, sizeof(LA), 1, inFile) != 0)
+    // open the writefile
+    FILE *outFile = fopen(argv[2], "wb");
+    if(outFile == NULL)
     {
-        printf("%lx\n", LA);
+        fprintf(stderr, "ERROR: can not open write the file\n");
+        exit(EXIT_FAILURE);
     }
-    printf("\n");
 
-    return 0;
+    // read file and get the corresponding LA and PA
+    while( fread(&LA, sizeof(LA), 1, inFile) != 0 )
+    {
+        dnum = LA & 0x7F;
+        pnum = LA >> d;
+        fnum = PT[pnum];
+        PA = (fnum << d) + dnum;
+        printf("The LA is %-4lx and Translated PA is %-4lx\n", LA, PA);
+    }
+
+    return EXIT_SUCCESS;
 }
---
+++ OS_Program/assignment/assign3/part2.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+typedef struct page_table
+{
+    unsigned int pt_fnum;
+    int vi;
+}page_table;
+
+int find_empty_frame(int *numlist, int length)
+{
+    int i = 0;
+    for(i=1; i<length; i++)
+    {
+        // we find a empty frame, return the index of the frame
+        if(numlist[i] == 1)
+        {
+            numlist[i] = 0;
+            return i;
+        }
+    }
+
+    // no empty frame
+    return -1;
+}
+
+int LRU_find(int *numlist, int length)
+{
+    int i = 0;
+    int min = numlist[1];
+
+    for(i=1; i<length; i++)
+    {
+        if(min > numlist[i])
+        {
+            min = numlist[i];
+        }
+    }
+
+    for(i=1; i<length; i++)
+    {
+        if(min == numlist[i])
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main(int argc, char *argv[])
+{
+    // if(argc < 3)
+    // {
+    //     fprintf(stderr, "Usage: ./part1 infile outfile.\n");
+    //     return 1;
+    // }
+
+    unsigned long LA, PA;
+    unsigned int p=5, f=3, d=7;
+    unsigned int pnum, fnum, dnum;
+
+    int CLK = 0, i = 0;
+    int freeframe[8] = {0,1,1,1,1,1,1,1};
+    int LRUcount[8] = {0};
+    int revmap[8] = {-1};
+
+    page_table PT[32];
+    for(i=0; i<32; i++)
+    {
+        PT[i].vi = 0;
+    }
+
+    // open the file 
+    FILE *inFile = fopen("part2sequence","rb");
+    if(inFile == NULL)
+    {
+        fprintf(stderr, "ERROR: can not read the file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // // open the writefile
+    // FILE *outFile = fopen(argv[2], "wb");
+    // if(outFile == NULL)
+    // {
+    //     fprintf(stderr, "ERROR: can not open write the file\n");
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // read file and get the corresponding LA and PA
+    int x = 0;
+    int y = 0;
+    int page_fault = 0;
+
+    while( fread(&LA, sizeof(LA), 1, inFile) != 0 )
+    {
+        CLK++;
+        dnum = LA & 0x7F;
+        pnum = LA >> d;
+        if(PT[pnum].vi == 1) // valid bit 
+        {
+            fnum = PT[pnum].pt_fnum;
+            PA = (fnum << d) + dnum;
+            LRUcount[fnum] = CLK;
+            // printf("The LA is %-4lx and Translated PA is %-4lx\n", LA, PA); // need write here
+        }
+        else
+        {
+            page_fault++;
+            x = find_empty_frame(freeframe, 8);
+            if(x > 0)
+            {
+                PT[pnum].pt_fnum = x;
+                PT[pnum].vi = 1;
+                fnum = PT[pnum].pt_fnum;
+                PA = (fnum << d) + dnum;
+                revmap[x] = pnum;
+                LRUcount[fnum] = CLK;
+                // printf("The LA is %-4lx and Translated PA is %-4lx\n", LA, PA); // need write here
+            }
+            else // no empty frame, run LRU
+            {
+                y = LRU_find(LRUcount, 8);
+                PT[revmap[y]].vi = 0;
+                PT[pnum].pt_fnum = y;
+                PT[pnum].vi = 1;
+                fnum = PT[pnum].pt_fnum;
+                PA = (fnum << d) + dnum;
+                LRUcount[fnum] = CLK;
+                revmap[fnum] = pnum;
+                // printf("The LA is %-4lx and Translated PA is %-4lx\n", LA, PA); // need write here
+            }
+        }
+    }
+
+    printf("%d\n", page_fault);
+    return 0;
+}
